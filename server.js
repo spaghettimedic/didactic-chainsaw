1f30c8992c9fca7fe460f112f6594377411dbaec
spaghettimedic <dsteiner247@gmail.com>
2022-01-27 08:37:31 -0500

    cookie given maxAge of 5 minutes

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
   secret: process.env.SECRET,
-  cookie: {},
+  cookie: {
+    maxAge: 300000
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
